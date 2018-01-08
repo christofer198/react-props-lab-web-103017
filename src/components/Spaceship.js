0aae2aba68a5a1b3b2902ce28ba215e069e2b6b0
Christofer Johnson <christofer.d.johnson@gmail.com>
2018-01-08 16:13:13 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,7 +1,19 @@
 // Code The Spaceship Component Here
 import React from 'react'
-const Spaceship = (data) => {
-  
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div className="movie-card">
+        <img
+          src={this.props.poster}
+          alt={this.props.title}
+        />
+        <h2>{this.props.title}</h2>
+        <small>Genres: {this.props.genres.join(', ')}</small>
+      </div>
+    );
+  }
 }
 
 export default Spaceship;
